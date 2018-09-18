91b4ca50fe5772db2c45d64f4d705fbc2ddf1733
Justin Daniel <justinddaniel@gmail.com>
2018-09-18 13:41:31 -0400

    Placed root_url and fetchWeather() function in actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,14 @@
 const API_KEY = 'insert key here';
+const ROOT_URL = 'insert root url to api you will call'; // 'http://api.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
 
 // middleware: function that lets action pass, manipulates it, logs it, or stops it, before it is given to reducers.
 // middleware can be thought of as gatekeepers.
 // all actions created flow through middleware.
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'; // use variable to keep types consistent between files that can be referenced later.
+
+export function fetchWeather() {
+  return {
+    type: FETCH_WEATHER
+  };
+}
