dc2ca165fdb661deec6275943e5bb0df8fcd66ad
Justin Daniel <justinddaniel@gmail.com>
2018-09-18 13:09:49 -0400

    Wired up what to do onSubmit; used event.preventDefault()

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -11,11 +11,16 @@ export default class SearchBar extends Component {
 
   onInputChange(event) {
     console.log(event.target.value);
-    this.setState({ term: event.target.value});
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault(); // using to prevent the form from submitting as a post request when button is pressed or hitting enter.
+    
   }
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group"> // although we are not submitting the form in browser, we are getting some functionality such as a searchbar input.
         <input
           placeholder="Get a five day forecast in your favorite citiees"
           className="form-control"
@@ -25,7 +30,7 @@ export default class SearchBar extends Component {
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
-      </form>
+      </form> // when you press enter on a child of form element, the browser automatically thinks it is submitting a form request through browser.
     );
   }
 }
