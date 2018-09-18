271710b233d35e3c38cdfdd4e4b3c8c1e6adf162
Justin Daniel <justinddaniel@gmail.com>
2018-09-18 13:32:00 -0400

    Wired index.js top parent in src; installed and imported redux-promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,5 @@
 const API_KEY = 'insert key here';
+
+// middleware: function that lets action pass, manipulates it, logs it, or stops it, before it is given to reducers.
+// middleware can be thought of as gatekeepers.
+// all actions created flow through middleware.
