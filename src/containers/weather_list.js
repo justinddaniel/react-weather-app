cd81006602e4d7ed672ce29568c815fd50f4f385
Justin Daniel <justinddaniel@gmail.com>
2018-09-19 13:56:45 -0400

    wired weather_list to return city name from api

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends Component { // export default is done down below when exporting the connect of mapStateToProps and WeatherList
+
+  renderWeather(cityData) {
+
+    const name = cityData.city.name; // structure of this determined by api data returned.
+
+    return (
+      <tr key={name}>
+        <td>{name}</td> 
+      </tr>
+    )
+  }
+
   render() {
     return (
         <table className="table table-hover">
@@ -13,7 +25,7 @@ class WeatherList extends Component { // export default is done down below when 
               <th>Humidity</th>
             </thead>
           <tbody>
-
+            { this.props.weather.map(this.renderWeather)}
           </tbody>
         </table>
     );
