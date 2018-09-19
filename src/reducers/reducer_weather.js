9067fd9cc69c1aaaa965353854f1878d69230856
Justin Daniel <justinddaniel@gmail.com>
2018-09-19 13:30:33 -0400

    wired reducer_weather to return payload data with state

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,10 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) { // weather data will be stored in array, and thus we start state as an empty array.
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return [ action.payload.data, ...state ];  // code to avoid: state.push(action.payload.data) because it mutates state
+  }
   console.log('Action reeived', action);
   return state;
 }
