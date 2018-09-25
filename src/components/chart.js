6c19549fc51d987e18094e3f13c85562289e241c
Justin Daniel <justinddaniel@gmail.com>
2018-09-25 14:29:53 -0400

    Added lodash. Wiring up average trendline and function average in chart.js

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Sparklines, SparklinesLine } from 'react-sparklines';
+import {Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 export default (props) => {
   return (
     <div>
       <Sparklines height={120} width=|180} data={props.data}> //props passed in from parent component weather_list
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
     </div>
   )
